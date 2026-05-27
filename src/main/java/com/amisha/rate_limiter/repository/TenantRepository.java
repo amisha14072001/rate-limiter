70e892d0c157e6f38631b88a9665662b8b7a05f9
amisha patel <apatel72@gmu.edu>
2026-05-27 13:25:09 -0400

    feat: add TenantRepository with findByTenantKey method

---
+++ src/main/java/com/amisha/rate_limiter/repository/TenantRepository.java
@@ -0,0 +1,16 @@
+package com.amisha.ratelimiter.repository;
+
+import com.amisha.ratelimiter.entity.Tenant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+
+public interface TenantRepository extends JpaRepository<Tenant, Long> {
+    Optional<Tenant> findByTenantKey(String tenantKey);
+}
+
+
+
+
